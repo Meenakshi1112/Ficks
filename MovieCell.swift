f3eeb03ca47aab868f34e726a835a5c89d011257
Meenakshi1112 <mmd329@msstate.edu>
2016-01-25 00:22:33 -0600

    Create MovieCell.swift

---
+++ MovieCell.swift
@@ -0,0 +1,26 @@
+ MovieCell.swift
+//  MovieViewer
+//
+//  Created by user115593 on 1/24/16.
+//  Copyright © 2016 user115593. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
